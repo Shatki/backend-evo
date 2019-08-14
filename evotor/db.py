c784f624fcd5dad690ad927f6bced34ff9bd7e00
Shatki <Shatki@mail.ru>
2019-08-14 09:14:17 +0300

    Разрабатываем кастомные поля модели

--- evotor/db.py
+++ evotor/db.py
@@ -1,36 +1,86 @@
 # -*- coding: utf-8 -*-
 import django.db.models as models
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
 import re
 
-DEFAULT_USERID = u'01-0000000000000001'
+
+class UserId:
+    """
+
+    """
+    DEFAULT_USERID = u'01-0000000000000001'
+
+    def __init__(self, fields=None,):
+        """
+            Создаем UserID
+            Примеры:
+                UserID('01-0000000000012345')
+                UserID(fields=('01', '0000000000012345')
+
+        """
+        pass
+
+    def __str__(self):
+        hex = '%032x' % self.int
+        return '%s-%s-%s-%s-%s' % (
+            hex[:8], hex[8:12], hex[12:16], hex[16:20], hex[20:])
 
 
-class UserIdField(models.BigIntegerField):
+    def default(self):
+        return self.DEFAULT_USERID
+
+
+class UserIdField(models.Field):
     """
         ID field uses in evotor users' model
     """
+    default_error_messages = {
+        'invalid': _('“%(value)s” is not a valid UserID.'),
+    }
+    description = _('Universally Evotor unique user identifier')
+    empty_strings_allowed = False
+
     DEFAULT_MASK = '00-000000000000000'
     PATTERN_USERID = r'[0-9]{2}-[0-9]{15}'
     SEPARATOR_CONST = '-'
-    description = "Evotor UserId model object"
 
     def __init__(self, *args, **kwargs):
-        kwargs['null'] = False
+        kwargs['max_length'] = 18
         super(UserIdField, self).__init__(*args, **kwargs)
 
     def deconstruct(self):
         print 'deconstruct'
         name, path, args, kwargs = super(UserIdField, self).deconstruct()
-        # del kwargs["max_length"], kwargs["null"]
+        del kwargs["max_length"]
         return name, path, args, kwargs
-
-    # def db_type(self, connection):
-    #     return 'userId'
 
     def get_internal_type(self):
         print 'get_internal_type'
         return 'UserIdField'
+
+    def get_prep_value(self, value):
+        print 'get_prep_value: ', value
+        value = super(UserIdField, self).get_prep_value(value)
+        if value is None:
+            return None
+
+        return self.to_python(value)
+
+    def get_db_prep_value(self, value, connection, prepared=False):
+        """
+            Convert value to db UserId BigInteger before save
+        """
+        # Преобразует val в значение для бэкенда базы данных. По умолчанию возвращает value,
+        # если prepared=True, иначе – результат get_prep_value()
+        print 'get_db_prep_value: ', value
+        if value is None:
+            return None
+        if re.match(self.PATTERN_USERID, value) and len(value) == 18:
+            # Возвращаем значения в базу
+            return value.replace('-', "").lstrip('0')
+        return str(value)
 
     def to_python(self, val):
         """
@@ -50,28 +100,13 @@ class UserIdField(models.BigIntegerField):
         else:
             raise ValidationError(u'Unknown DB data format value')
 
-    def get_prep_value(self, val, *args, **kwargs):
-        """
-            Convert value to db UserId BigInteger before save
-        """
-        # Преобразует val в значение для бэкенда базы данных. По умолчанию возвращает value,
-        # если prepared=True, иначе – результат get_prep_value()
-        print 'get_prep_value: ', val
-        if val is None:
-            raise ValidationError(u'The value cannot to be is None')
-        if re.match(self.PATTERN_USERID, val) and len(val) == 18:
-            # Возвращаем значения в базу
-            return val.replace('-', "").lstrip('0')
-        raise ValidationError(u'Bad value')
-
     def value_to_string(self, obj):
         # Преобразование значения поля для сериалайзера
-        print 'value_to_string: ', obj, self._get_val_from_obj(obj)
+        print 'value_to_string: ', obj, self.get_val_from_obj(obj)
         # obj: 01-000000000738894 <class 'users.models.User'>
-        val = self._get_val_from_obj(obj)
         # val: 01-000000000738894 <type 'unicode'>
         # Преобразование в читаемую форму
-        return val
+        return str(self.value_from_object(obj))
 
     def from_db_value(self, val, *args, **kwargs):
         print 'from_db_value: ', val, self.to_python(val)
