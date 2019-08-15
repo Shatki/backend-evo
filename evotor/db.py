409a4472380bae6d6c7743fe41ccbdef32ec346e
Shatki <Shatki@mail.ru>
2019-08-15 09:29:29 +0300

    Разрабатываем кастомные поля модели

--- evotor/db.py
+++ evotor/db.py
@@ -6,30 +6,95 @@ from django.utils.translation import gettext_lazy as _
 import re
 
 
-class UserId:
+class UserId(object):
     """
 
     """
-    DEFAULT_USERID = u'01-0000000000000001'
+    DEFAULT_USERID = '01-0000000000000001'
+    DEFAULT_MASK = '00-000000000000000'
+    PATTERN_USERID = r'[0-9]{2}-[0-9]{15}'
+    SEPARATOR_CONST = '-'
+    MAX_NODE = 10**2
+    MAX_NUMBER = 10**15
+    MAX_INT = MAX_NODE * MAX_NUMBER
 
-    def __init__(self, fields=None,):
+    def __init__(self, str=None, int=None, fields=None,):
         """
             Создаем UserID
             Примеры:
                 UserID('01-0000000000012345')
                 UserID(fields=('01', '0000000000012345')
 
+
+            UserIds have these read-only attributes:
+
+                fields      a tuple of the two integer fields of the UserId,
+                            which are also available as two individual attributes
+                            and two derived attributes:
+                    node                    the first 32 bits of the UserId
+                    number                  the next 16 bits of the UserId
+
+                str         the UUID as a 18-character decimal string
+                int         the UUID as a long integer
+
         """
-        pass
+        # должен быть получен только один из аргументов
+        if [str, int, fields].count(None) != 2:
+            raise TypeError('one of the str, fields, '
+                            'or int arguments must be given')
+
+        # Если получили fields
+        if fields is not None:
+            if len(fields) != 2:
+                raise ValueError('fields is not a 2-tuple')
+            (node, number) = fields
+            if not 0 < node < self.MAX_NODE:
+                raise ValueError('field 1 out of range (must be between 1 and 99)')
+            if not 0 < number < self.MAX_NUMBER:
+                raise ValueError('field 1 out of range (must be between 1 and 10**15-1)')
+            int = node * self.MAX_NUMBER + number
+
+        if str is not None:
+            # TODO: В третьей версии питона переделать long на int
+            int = long(str.replace(self.SEPARATOR_CONST, "").lstrip('0'))
+
+        if int is not None:
+            if not 0 < int < self.MAX_INT:
+                raise ValueError('int is out of range (must be between 1 and 10**17-1)')
+
+        object.__setattr__(self, 'int', int)
+
+    @property
+    def node(self):
+        return '%017d' % self.int[:2]
+
+    @property
+    def number(self):
+        return '%017d' % self.int[2:]
+
+    @property
+    def fields(self):
+        str = '%017d' % self.int
+        return str[0:2], str[2:18]
+
+    def __hash__(self):
+        return hash(self.int)
+
+    def __int__(self):
+        return self.int
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, str(self))
+
+    def __setattr__(self, name, value):
+        raise TypeError('UserId objects are immutable')
 
     def __str__(self):
-        hex = '%032x' % self.int
-        return '%s-%s-%s-%s-%s' % (
-            hex[:8], hex[8:12], hex[12:16], hex[16:20], hex[20:])
-
+        str = '%017d' % self.int
+        return '%s-%s' % (str[:2], str[2:])
 
     def default(self):
-        return self.DEFAULT_USERID
+        return UserId(str=self.DEFAULT_USERID)
 
 
 class UserIdField(models.Field):
@@ -114,3 +179,8 @@ class UserIdField(models.Field):
         return self.to_python(val)
 
 
+print UserId(fields=(1, 12345))
+print UserId(str='01-000000000023456')
+print UserId(int=1000000000034567)
+
+print UserId.fields
